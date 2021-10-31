222c654d9bd3481208b98d52cb535618618fcf8f
cristina.ionescu@gsdgroup.net <cristina.ionescu@gsdgroup.net>
2021-10-31 21:35:58 +0200

    install Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [
+    './src/**/*.html',
+    './src/**/*.css',
+    './src/**/*.ts'
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
